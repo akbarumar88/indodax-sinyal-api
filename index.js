0c251a5b88f94d2d13a182e7b71c29a8f38c34db
Akbar Umar Alfaro <akbarumar88@gmail.com>
2022-06-06 23:44:52 +0700

    - Tambah Filter untuk masing-masing kolom

--- index.js
+++ index.js
@@ -21,19 +21,65 @@ app.listen(PORT, () => {
 })
 
 app.get("/all", async (req, res, next) => {
-  const { page = 1, perpage = 10, tglawal, tglakhir } = req.query
+  const {
+    page = 1,
+    perpage = 10,
+    tglawal,
+    tglakhir,
+    hargaUSDTDari,
+    hargaUSDTSampai,
+    hargaIDRDari,
+    hargaIDRSampai,
+    volUSDTDari,
+    volUSDTSampai,
+    volIDRDari,
+    volIDRSampai,
+    lastBuyDari,
+    lastBuySampai,
+    lastSellDari,
+    lastSellSampai,
+  } = req.query
   let filterTgl =
     !empty(tglawal) && !empty(tglakhir)
       ? `AND tanggal BETWEEN '${tglawal} 00:00:00' AND '${tglakhir} 23:59:59'`
       : ""
+
+  let filterHargaUSDT =
+    !empty(hargaUSDTDari) && !empty(hargaUSDTSampai)
+      ? `AND hargausdt BETWEEN ${hargaUSDTDari} AND ${hargaUSDTSampai}`
+      : ""
+
+  let filterHargaIDR =
+    !empty(hargaIDRDari) && !empty(hargaIDRSampai)
+      ? `AND hargaidr BETWEEN ${hargaIDRDari} AND ${hargaIDRSampai}`
+      : ""
+
+  let filterVolUSDT =
+    !empty(volUSDTDari) && !empty(volUSDTSampai)
+      ? `AND volusdt BETWEEN ${volUSDTDari} AND ${volUSDTSampai}`
+      : ""
+
+  let filterVolIDR =
+    !empty(volIDRDari) && !empty(volIDRSampai)
+      ? `AND volidr BETWEEN ${volIDRDari} AND ${volIDRSampai}`
+      : ""
+
+  let filterLastBuy =
+    !empty(lastBuyDari) && !empty(lastBuySampai)
+      ? `AND lastbuy BETWEEN ${lastBuyDari} AND ${lastBuySampai}`
+      : ""
+  let filterLastSell =
+    !empty(lastSellDari) && !empty(lastSellSampai)
+      ? `AND lastsell BETWEEN ${lastSellDari} AND ${lastSellSampai}`
+      : ""
   // console.log({page,perpage})
   let offset = (page - 1) * perpage
-  let data = await db.query(
-    `SELECT * FROM btc WHERE TRUE ${filterTgl} LIMIT ${perpage} OFFSET ${offset}`
-  )
-  let additional = await db.query(
-    `SELECT COUNT(id) as jml FROM btc WHERE TRUE ${filterTgl}`
-  )
+  let query = `SELECT * FROM btc WHERE TRUE ${filterTgl} ${filterHargaUSDT} ${filterHargaIDR} ${filterVolUSDT} ${filterVolIDR} ${filterLastBuy} ${filterLastSell} LIMIT ${perpage} OFFSET ${offset}`
+  console.log(query)
+  let data = await db.query(query)
+
+  let queryCount = `SELECT COUNT(id) as jml FROM btc WHERE TRUE ${filterTgl} ${filterHargaUSDT} ${filterHargaIDR} ${filterVolUSDT} ${filterVolIDR} ${filterLastBuy} ${filterLastSell}`
+  let additional = await db.query(queryCount)
   let dataCount = additional[0].jml
   let pageCount = Math.ceil(dataCount / perpage)
   res.json({
