3c50a997fcb99c9c77c0156884ccca2756f29a4d
Akbar Umar Alfaro <akbarumar88@gmail.com>
2022-06-22 13:09:21 +0700

    - Perbaiki CORS

--- index.js
+++ index.js
@@ -18,33 +18,33 @@ const whitelist = [
   "http://localhost:3000",
   "https://indodax-sinyal-app.herokuapp.com/",
 ]
-app.use(cors({ ...corsOptions, methods: "GET,PUT,POST,DELETE" })) // Use this after the variable declaration
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // res.setHeader(
-  //   "Access-Control-Allow-Origin",
-  //   "https://webdokter.herokuapp.com"
-  // );
-  const origin = req.headers.origin
-  if (whitelist.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin)
-  }
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  )
+app.use(cors()) // Use this after the variable declaration
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Origin",
+//   //   "https://webdokter.herokuapp.com"
+//   // );
+//   const origin = req.headers.origin
+//   if (whitelist.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin)
+//   }
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   )
 
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+//   // Request headers you wish to allow
+//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type")
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true)
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true)
 
-  // Pass to next layer of middleware
-  next()
-})
+//   // Pass to next layer of middleware
+//   next()
+// })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
